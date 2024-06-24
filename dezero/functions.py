ce08ec59314d2fcbbf5a4a77346224abd0788782
tang <876836336@qq.com>
2024-06-24 13:55:26 +0800

    step 42-43: linear & neural network

--- dezero/functions.py
+++ dezero/functions.py
@@ -5,7 +5,7 @@ from dezero.core import Function, Variable, as_variable, as_array
 
 
 # =============================================================================
-# Basic functions: sin / cos / tanh
+# Basic functions: sin / cos / tanh / exp / log
 # =============================================================================
 class Sin(Function):
     def forward(self, x):
@@ -52,6 +52,36 @@ def tanh(x):
     return Tanh()(x)
 
 
+class Exp(Function):
+    def forward(self, x):
+        y = np.exp(x)
+        return y
+    
+    def backward(self, gy):
+        y = self.outputs[0]()  # weakref
+        gx = gy * y
+        return gx
+    
+
+def exp(x):
+    return Exp()(x)
+
+
+class Log(Function):
+    def forward(self, x):
+        y = np.log(x)
+        return y
+    
+    def backward(self, gy):
+        x, = self.inputs
+        gx = gy / x
+        return gx
+
+
+def log(x):
+    return Log()(x)
+
+
 # =============================================================================
 # tensor operations: reshape / transpose
 # =============================================================================
@@ -96,7 +126,7 @@ def transpose(x, axes=None):
 
 
 # =============================================================================
-# tensor operations: sum / sum_to / broadcast_to / average / matmul
+# tensor operations: sum / sum_to / broadcast_to / average / matmul / linear
 # =============================================================================
 class Sum(Function):
     def __init__(self, axis=None, keepdims=False):
@@ -179,3 +209,75 @@ class MatMul(Function):
 
 def matmul(x, W):
     return MatMul()(x, W)
+
+
+class Linear(Function):
+    def forward(self, x, W, b):
+        y = x.dot(W)  # ndarray的矩阵乘法
+        if b is not None:  # 如果有偏置，加上偏置
+            y += b
+        return y
+
+    def backward(self, gy):  # 加法和矩阵乘法的反向传播
+        x, W, b = self.inputs
+        gb = None if b.data is None else sum_to(gy, b.shape)
+        gx = matmul(gy, W.T)
+        gW = matmul(x.T, gy)
+        return gx, gW, gb
+
+
+def linear(x, W, b=None):
+    return Linear()(x, W, b)
+
+
+# =============================================================================
+# loss functions: mean_squared_error
+# =============================================================================
+def mean_squared_error_simple(x0, x1):
+    x0, x1 = as_variable(x0), as_variable(x1)
+    diff = x0 - x1  # 中间Variable变量diff
+    y = sum(diff ** 2) / len(diff)
+    return y
+
+
+class MeanSquaredError(Function):  # 封装成Function减少中间变量内存
+    def forward(self, x0, x1):
+        diff = x0 - x1  # 这里的diff的类型是ndarray
+        y = (diff ** 2).sum() / len(diff)
+        return y
+
+    def backward(self, gy):
+        x0, x1 = self.inputs
+        diff = x0 - x1
+        gx0 = gy * diff * (2. / len(diff))
+        gx1 = -gx0
+        return gx0, gx1
+
+
+def mean_squared_error(x0, x1):
+    return MeanSquaredError()(x0, x1)
+
+
+# =============================================================================
+# activation functions: sigmoid
+# =============================================================================
+def sigmoid_simple(x):
+    x = as_variable(x)
+    y = 1 / (1 + exp(-x))
+    return y
+
+
+class Sigmoid(Function):
+    def forward(self, x):
+        y = 1 / (1 + exp(-x))
+        # y = tanh(x * 0.5) * 0.5 + 0.5  # Better implementation
+        return y
+
+    def backward(self, gy):
+        y = self.outputs[0]()
+        gx = gy * y * (1 - y)
+        return gx
+
+
+def sigmoid(x):
+    return Sigmoid()(x)
