67e545b884ddfe2993777b89ac8050325967a7b3
tang <876836336@qq.com>
2024-07-26 11:47:54 +0800

    step 48: multi-class

--- dezero/utils.py
+++ dezero/utils.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import numpy as np
 
 
 # =============================================================================
@@ -125,3 +126,28 @@ def reshape_sum_backward(gy, x_shape, axis, keepdims):
 
     gy = gy.reshape(shape)  # reshape
     return gy
+
+
+def logsumexp(x, axis=1):
+    # xp = cuda.get_array_module(x)
+    m = x.max(axis=axis, keepdims=True)
+    y = x - m
+    # xp.exp(y, out=y)
+    np.exp(y, out=y)
+    s = y.sum(axis=axis, keepdims=True)
+    # xp.log(s, out=s)
+    np.log(s, out=s)
+    m += s
+    return m
+
+
+def max_backward_shape(x, axis):
+    if axis is None:
+        axis = range(x.ndim)
+    elif isinstance(axis, int):
+        axis = (axis,)
+    else:
+        axis = axis
+
+    shape = [s if ax not in axis else 1 for ax, s in enumerate(x.shape)]
+    return shape
