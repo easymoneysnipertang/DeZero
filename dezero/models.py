be02980534623bb1e7e276fc95cd55a1908417af
tang <876836336@qq.com>
2024-07-31 19:46:13 +0800

    step 58: VGG16

--- dezero/models.py
+++ dezero/models.py
@@ -2,6 +2,7 @@ from dezero import Layer
 from dezero import utils
 import dezero.functions as F
 import dezero.layers as L
+import numpy as np
 
 
 # =============================================================================
@@ -46,3 +47,69 @@ class MLP(Model):
         for l in self.layers[:-1]:  # 前n-1层使用激活函数
             x = self.activation(l(x))
         return self.layers[-1](x)  # 最后一层不使用激活函数
+    
+
+# =============================================================================
+# VGG
+# =============================================================================
+class VGG16(Model):
+    WEIGHTS_PATH = 'https://github.com/koki0702/dezero-models/releases/download/v0.1/vgg16.npz'
+
+    def __init__(self, pretrained=False):
+        super().__init__()
+        self.conv1_1 = L.Conv2d(64, kernel_size=3, stride=1, pad=1)
+        self.conv1_2 = L.Conv2d(64, kernel_size=3, stride=1, pad=1)
+        self.conv2_1 = L.Conv2d(128, kernel_size=3, stride=1, pad=1)
+        self.conv2_2 = L.Conv2d(128, kernel_size=3, stride=1, pad=1)
+        self.conv3_1 = L.Conv2d(256, kernel_size=3, stride=1, pad=1)
+        self.conv3_2 = L.Conv2d(256, kernel_size=3, stride=1, pad=1)
+        self.conv3_3 = L.Conv2d(256, kernel_size=3, stride=1, pad=1)
+        self.conv4_1 = L.Conv2d(512, kernel_size=3, stride=1, pad=1)
+        self.conv4_2 = L.Conv2d(512, kernel_size=3, stride=1, pad=1)
+        self.conv4_3 = L.Conv2d(512, kernel_size=3, stride=1, pad=1)
+        self.conv5_1 = L.Conv2d(512, kernel_size=3, stride=1, pad=1)
+        self.conv5_2 = L.Conv2d(512, kernel_size=3, stride=1, pad=1)
+        self.conv5_3 = L.Conv2d(512, kernel_size=3, stride=1, pad=1)
+        self.fc6 = L.Linear(4096)
+        self.fc7 = L.Linear(4096)
+        self.fc8 = L.Linear(1000)
+
+        if pretrained:
+            weights_path = utils.get_file(VGG16.WEIGHTS_PATH)
+            self.load_weights(weights_path)
+
+    def forward(self, x):
+        x = F.relu(self.conv1_1(x))
+        x = F.relu(self.conv1_2(x))
+        x = F.pooling(x, 2, 2)
+        x = F.relu(self.conv2_1(x))
+        x = F.relu(self.conv2_2(x))
+        x = F.pooling(x, 2, 2)
+        x = F.relu(self.conv3_1(x))
+        x = F.relu(self.conv3_2(x))
+        x = F.relu(self.conv3_3(x))
+        x = F.pooling(x, 2, 2)
+        x = F.relu(self.conv4_1(x))
+        x = F.relu(self.conv4_2(x))
+        x = F.relu(self.conv4_3(x))
+        x = F.pooling(x, 2, 2)
+        x = F.relu(self.conv5_1(x))
+        x = F.relu(self.conv5_2(x))
+        x = F.relu(self.conv5_3(x))
+        x = F.pooling(x, 2, 2)
+        x = F.reshape(x, (x.shape[0], -1))  # 变形以实现linear
+        x = F.dropout(F.relu(self.fc6(x)))
+        x = F.dropout(F.relu(self.fc7(x)))
+        x = self.fc8(x)
+        return x
+
+    @staticmethod
+    def preprocess(image, size=(224, 224), dtype=np.float32):
+        image = image.convert('RGB')  # 转换为RGB格式
+        if size:
+            image = image.resize(size)
+        image = np.asarray(image, dtype=dtype)
+        image = image[:, :, ::-1]
+        image -= np.array([103.939, 116.779, 123.68], dtype=dtype)  # 减去均值
+        image = image.transpose((2, 0, 1))  # 转换为CHW格式
+        return image
